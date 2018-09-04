196a8581cb49378b6165cccdc03440a0736753c0
Qclaudie <42715820+Qclaudie@users.noreply.github.com>
2018-09-04 11:58:49 -0400

    Update Login.php

--- cop4331contacts.com/public_html/API/Login.php
+++ cop4331contacts.com/public_html/API/Login.php
@@ -13,7 +13,7 @@ SELECT userID, firstName, lastName, login FROM users WHERE login = '<login>' AND
 	$lastName = "";
 	//$login = "";
 
-	$conn = new mysqli("167.99.12.10", "claudie", "orlando", "contactBook");
+	$conn = new mysqli_connect("167.99.12.10", "claudie", "orlando");
 	
 	if ($conn->connect_error) 
 	{
