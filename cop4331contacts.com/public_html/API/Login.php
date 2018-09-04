16736470b24dce5b4f41fa4cf1502610fab5c815
Qclaudie <42715820+Qclaudie@users.noreply.github.com>
2018-09-04 11:18:01 -0400

    Add files via upload

---
+++ cop4331contacts.com/public_html/API/Login.php
@@ -0,0 +1,69 @@
+<?php
+/*
+
+Login:
+
+SELECT userID, firstName, lastName, login FROM users WHERE login = '<login>' AND password = '<hashed password>';
+*/
+
+	$inData = getRequestInfo();
+	
+	$userID = 0;
+	$firstName = "";
+	$lastName = "";
+	//$login = "";
+
+	$conn = new mysqli("167.99.12.10", "claudie", "orlando", "contactBook");
+	
+	if ($conn->connect_error) 
+	{
+		returnWithError( $conn->connect_error );
+	} 
+	else
+	{
+		/*Login is the weak point for SQL injection, assuming the password is sent hashed*/
+		$sql = "SELECT userID, firstName, lastName, login FROM users WHERE login = '".$inData["login"]."' AND password = '".$inData[password]."'";
+		
+		$result = $conn->query($sql);
+		
+		if ($result->num_rows > 0)
+		{
+			$row = $result->fetch_assoc();
+			$firstName = $row["firstName"];
+			$lastName = $row["lastName"];
+			$userID = $row["userID"];
+			//$login = $row["login"];
+			
+			returnWithInfo($firstName, $lastName, $userID);
+		}
+		else
+		{
+			returnWithError( "No Records Found" );
+		}
+		$conn->close();
+	}
+	
+	function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
+
+	function sendResultInfoAsJson( $obj )
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+	
+	function returnWithError( $err )
+	{
+		$retValue = '{"userID":0,"firstName":"","lastName":"","error":"' . $err . '"}';
+		sendResultInfoAsJson( $retValue );
+	}
+	
+	function returnWithInfo( $firstName, $lastName, $userID )
+	{
+		$retValue = '{"userID":' . $userID . ',"firstName":"' . $firstName . '","lastName":"' . $lastName . '","error":""}';
+		sendResultInfoAsJson( $retValue );
+	}
+	
+?>
---
+++ cop4331contacts.com/public_html/API/Register.php
@@ -0,0 +1,53 @@
+<?php
+
+/*
+	Register:
+
+INSERT INTO USERS (firstName, lastName, login, password) VALUES ('<firstName>', '<lastName>', '<login/email>', '<hashed password>');
+
+
+*/
+
+	$inData = getRequestInfo();
+	
+	
+	$conn = new mysqli("167.99.12.10", "claudie", "orlando", "contactBook");
+	
+	if ($conn->connect_error) 
+	{
+		returnWithError( $conn->connect_error );
+	} 
+	else
+	{
+		/*Need to check duplicate users?*/
+		/*firstName, lastName, and login are weakpoints that could be be SQL injected assumming password is hashed*/
+		$sql = "INSERT INTO USERS (firstName, lastName, login, password) VALUES ('".$inData["firstName"]."', '".$inData["lastName"]."', '".$inData["login"]."', '".$inData["password"]."');";
+		
+		
+		if( $result = $conn->query($sql) != TRUE )
+		{
+			returnWithError( $conn->error );
+		}
+		$conn->close();
+	}
+	
+	returnWithError("");
+	
+	function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
+
+	function sendResultInfoAsJson( $obj )
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+	
+	function returnWithError( $err )
+	{
+		$retValue = '{"error":"' . $err . '"}';
+		sendResultInfoAsJson( $retValue );
+	}
+	
+?>
