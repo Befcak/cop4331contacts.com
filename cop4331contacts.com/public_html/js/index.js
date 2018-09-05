8099b3c669a3e13213b651c73b7cc33a3beca2a7
Bridget Woodye <BrdgtColleenW@gmail.com>
2018-09-05 15:37:37 -0400

    Add model
    organized and added some comments to the html and the add contact model is done

--- cop4331contacts.com/public_html/js/index.js
+++ cop4331contacts.com/public_html/js/index.js
@@ -1,3 +1,13 @@
-$(document).ready(() => {
-  $('body').scrollspy({ target: '#navbar-example' })
-})
+function addContact() {
+  document.getElementById('myModal').style.display = 'block';
+}
+
+var span = document.getElementsByClassName("close")[0];
+
+span.onclick = function() {
+    document.getElementById('myModal').style.display = 'none';
+}
+
+function makeContact() {
+
+}
