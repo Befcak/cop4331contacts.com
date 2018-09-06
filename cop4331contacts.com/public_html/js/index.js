2ae9205eeff02d353aa89358b0a236b08e777b05
Bridget Woodye <BrdgtColleenW@gmail.com>
2018-09-05 23:40:45 -0400

    Add update
    Adds to a scrollable list and saves all information from model

--- cop4331contacts.com/public_html/js/index.js
+++ cop4331contacts.com/public_html/js/index.js
@@ -9,5 +9,32 @@ span.onclick = function() {
 }
 
 function makeContact() {
+    var li = document.createElement("li");
+    var first = document.getElementById("firstName").value;
+    var last = document.getElementById("lastName").value;
+    var phone = document.getElementById("phone").value;
+    var email = document.getElementById("email").value;
+    var streetAddress = document.getElementById("streetAddress");
+    var city = document.getElementById("city").value;
+    var state = document.getElementById("state").value;
+    var zip = document.getElementById("zip").value;
+    var birthday = document.getElementById("birth").value;
+    var note = document.getElementById("note").value;
 
+    var t = document.createTextNode(first +' '+ last);
+    li.appendChild(t);
+    if (first === '') {
+      alert("You must write something!");
+    } else {
+      document.getElementById("myUL").appendChild(li);
+    }
+    document.getElementById('firstName').value = firstName.defaultValue;
+    document.getElementById('lastName').value = lastName.defaultValue;
+
+    var span = document.createElement("SPAN");
+    var txt = document.createTextNode("\u00D7");
+
+    span.appendChild(txt);
+
+    document.getElementById('myModal').style.display = 'none';
 }
