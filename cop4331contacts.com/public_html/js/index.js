3c11d75c75e205282eb069798ace9e11204bff85
Bridget Woodye <BrdgtColleenW@gmail.com>
2018-09-06 16:34:07 -0400

    once again

--- cop4331contacts.com/public_html/js/index.js
+++ cop4331contacts.com/public_html/js/index.js
@@ -47,6 +47,7 @@ function doLogin()
 			return;
 		}
 
+		window.location.href="http://167.99.12.10/html/contactManager.html";
 		// Grabbing json reply with firstName and lastName.
 		firstName = jsonObject.firstName;
 		lastName = jsonObject.lastName;
