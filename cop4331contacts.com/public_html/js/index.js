8c75d9688ea184444156c4cedf69021956e03e0c
Bridget Woodye <BrdgtColleenW@gmail.com>
2018-09-06 15:58:00 -0400

    changing login button to change screens

--- cop4331contacts.com/public_html/js/index.js
+++ cop4331contacts.com/public_html/js/index.js
@@ -47,8 +47,6 @@ function doLogin()
 			return;
 		}
 
-		window.location.href = "html/contactManager.html";
-
 		// Grabbing json reply with firstName and lastName.
 		firstName = jsonObject.firstName;
 		lastName = jsonObject.lastName;
