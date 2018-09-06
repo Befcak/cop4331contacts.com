0b91a3c9a53aba03619daa100603fbcc760654be
ayylers <iehlers@hotmail.com>
2018-09-06 00:28:19 -0400

    Functionality Not Yet Tested

---
+++ cop4331contacts.com/public_html/API/AddContact.php
@@ -0,0 +1,51 @@
+<?php
+	$inData = getRequestInfo();
+	
+	// CHANGE DATABASE NAME? ("contactBook")
+	$userId = $inData["userId"];
+	$firstName = $inData["firstName"];
+	$lastName = $inData["lastName"];
+	$streetAddress = $inData["streetAddress"];
+	$city = $inData["city"];
+	$state = $inData["state"];
+	$zip = $inData["zip"];
+	$phone = $inData["phone"];
+	$email = $inData["email"];
+	$birthday = $inData["birthday"];
+	$notes = $inData["notes"];
+
+	$conn = new mysqli("localhost", "root", "orlando");
+	if ($conn->connect_error) 
+	{
+		returnWithError( $conn->connect_error );
+	} 
+	else
+	{
+		$sql = "INSERT INTO contacts (userID, firstName, lastName, streetAddress, city, state, zip, phone, email, birthday, notes) VALUES (<userID>, '<firstName>', '<lastName>', '<streetAddress>', '<city>', '<state>', <zip>, <phone>, '<email>', '<birthday>', '<notes>')";
+		if( $result = $conn->query($sql) != TRUE )
+		{
+			returnWithError( $conn->error );
+		}
+		$conn->close();
+	}
+	
+	returnWithError("");
+	
+	function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
+
+	function sendResultInfoAsJson( $obj )
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+	
+	function returnWithError( $err )
+	{
+		$retValue = '{"error":"' . $err . '"}';
+		sendResultInfoAsJson( $retValue );
+	}
+	
+?>
