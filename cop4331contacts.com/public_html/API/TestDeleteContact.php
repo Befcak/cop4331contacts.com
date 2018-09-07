187ffbae9526fa01fcc9831b3a00b63638561fe2
ayylers <iehlers@hotmail.com>
2018-09-07 14:27:59 -0400

    Update TestDeleteContact.php

--- cop4331contacts.com/public_html/API/TestDeleteContact.php
+++ cop4331contacts.com/public_html/API/TestDeleteContact.php
@@ -2,19 +2,26 @@
 	//TO DO: Link to front end (JavaScript) via getRequestInfo()
 	$inData = getRequestInfo();
 	
-	$userID = 2 //$inData["userID"];
-	$contactID = 11 // = $inData["contactID"];
-
+	$userID = $inData["userID"];
+	$firstName = $inData["firstName"];
+	$lastName = $inData["lastName"];
+	$streetAddress = $inData["streetAddress"];
+	$city = $inData["city"];
+	$state = $inData["state"];
+	$zip = $inData["zip"];
+	$phone = $inData["phone"];
+	$email = $inData["email"];
+	$birthday = $inData["birthday"];
+	$notes = $inData["notes"];
 	$conn = new mysqli("localhost", "root", "orlando", "contactBook");
-	
+
 	if ($conn->connect_error) 
 	{
 		returnWithError( $conn->connect_error );
 	} 
 	else
 	{	
-		$sql = "DELETE FROM contacts WHERE userID = ".$userID.", AND contactID = ".$contactID.";";
-
+		$sql = "INSERT INTO contacts (userID, firstName, lastName, streetAddress, city, state, zip, phone, email, birthday, notes) VALUES ('".$userID."', '".$firstName."', '".$lastName."', '".$streetAddress."', '".$city."', '".$state."', '".$zip."', '".$phone."', '".$email."', '".$birthday."', '".$notes."');";
 		if( $result = $conn->query($sql) != TRUE )
 		{
 			//returnWithError( $conn->error );
