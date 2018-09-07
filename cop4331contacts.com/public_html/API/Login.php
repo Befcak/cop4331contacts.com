65c54f471b8cca8d8b4553d3a2cb1d502a336d32
Qclaudie <42715820+Qclaudie@users.noreply.github.com>
2018-09-07 13:29:55 -0400

    Update Login.php

--- cop4331contacts.com/public_html/API/Login.php
+++ cop4331contacts.com/public_html/API/Login.php
@@ -42,10 +42,10 @@ SELECT userID, firstName, lastName, login FROM users WHERE login = '<login>' AND
 				$firstName = $row["firstName"];
 				$lastName = $row["lastName"];
 				$userID = $row["userID"];
-				$currentDate= getdate("U");
-				$dateString = "$currentDate[year]-$currentDate[mon]-$currentDate[mday] $currentDate[hours]:$currentDate[minutes]:$currentDate[seconds]";
-				
-				$sql2 = "UPDATE users SET dateLastLoggedIn = '$dateString' WHERE userID = $userID";
+				$currentDate= getdate();
+				$dateString = "".$currentDate[year]."-".$currentDate[mon]."-".$currentDate[mday]." ".$currentDate[hours].":".$currentDate[minutes].":".$currentDate[seconds]."";
+				returnWithError($dateString);
+				$sql2 = "UPDATE users SET dateLastLoggedIn = '".$dateString."' WHERE userID = $userID";
 				$conn->query($sql2);
 				
 				
