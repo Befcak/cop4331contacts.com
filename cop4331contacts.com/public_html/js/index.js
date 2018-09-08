bb1e31b6eef47a1cc22ccc4dbc622bbc66f5d862
Bridget Woodye <BrdgtColleenW@gmail.com>
2018-09-08 11:06:40 -0400

    connecting the java script
    It was one quotation.

--- cop4331contacts.com/public_html/js/index.js
+++ cop4331contacts.com/public_html/js/index.js
@@ -7,52 +7,51 @@ var lastName = "";
 
 function doLogin()
 {
-	userId = 0;
-	firstName = "";
-	lastName = "";
-
-	var login = document.getElementById("loginName").value;
-	var password = document.getElementById("loginPassword").value;
-
-	document.getElementById("loginResult").innerHTML = "";
-
-	var jsonPayload = '{"login" : "' + login + '", "password" : "' + password + '"}';
-	var url = urlBase + '/Login.' + extension;
-
-	var xhr = new XMLHttpRequest();
-	xhr.open("POST", url, false);
-	xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
-	try
-	{
-		xhr.send(jsonPayload);
-
-		var jsonObject = JSON.parse( xhr.responseText );
-
-		userId = jsonObject.id;
-
-		if( userId < 1 )
-		{
-			document.getElementById("loginResult").innerHTML = "User/Password combination incorrect";
-			return;
-		}
-
-		firstName = jsonObject.firstName;
-		lastName = jsonObject.lastName;
-
-		document.getElementById("userName").innerHTML = firstName + " " + lastName;
-
-		document.getElementById("loginName").value = "";
-		document.getElementById("loginPassword").value = "";
-
+	// userId = 0;
+	// firstName = "";
+	// lastName = "";
+	//
+	// var login = document.getElementById("loginName").value;
+	// var password = document.getElementById("loginPassword").value;
+	//
+	// document.getElementById("loginResult").innerHTML = "";
+	//
+	// var jsonPayload = '{"login" : "' + login + '", "password" : "' + password + '"}';
+	// var url = urlBase + '/Login.' + extension;
+	//
+	// var xhr = new XMLHttpRequest();
+	// xhr.open("POST", url, false);
+	// xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+	// try
+	// {
+	// 	xhr.send(jsonPayload);
+	//
+	// 	var jsonObject = JSON.parse( xhr.responseText );
+	//
+	// 	userId = jsonObject.id;
+	//
+	// 	if( userId < 1 )
+	// 	{
+	// 		document.getElementById("loginResult").innerHTML = "User/Password combination incorrect";
+	// 		return;
+	// 	}
+	//
+	// 	firstName = jsonObject.firstName;
+	// 	lastName = jsonObject.lastName;
+	//
+	// 	document.getElementById("userName").innerHTML = firstName + " " + lastName;
+	//
+	// 	document.getElementById("loginName").value = "";
+	// 	document.getElementById("loginPassword").value = "";
+	//
 		hideOrShow( "loggedInDiv", true);
 		hideOrShow( "accessUIDiv", true);
 		hideOrShow( "loginDiv", false);
-	}
-	catch(err)
-	{
-		document.getElementById("loginResult").innerHTML = err.message;
-	}
-
+	// }
+	// catch(err)
+	// {
+	// 	document.getElementById("loginResult").innerHTML = err.message;
+	// }
 }
 
 function doLogout()
@@ -122,8 +121,8 @@ function register()
 	var regFullName = document.getElementById("reg_fullname").value;
 	document.getElementById("registerResult").innerHTML = "";
 
-	var jsonPayload = '{"username" : "' + regUsername + '", "password" : ' + userId + '", "email" : ' + regEmail
-	+ '", "fullname" : ' + fullname '}';
+	var jsonPayload = '{"username" : "' + regUsername + '", "password" : "' + userId + '", "email" : "' + regEmail
+	+ '", "fullname" : + "' + fullname +'"}';
 	var url = urlBase + '/Register.' + extension;
 
 	var xhr = new XMLHttpRequest();
