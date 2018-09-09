2c8f503238c040404d3b72f4415d3e755784e7d8
Bridget Woodye <BrdgtColleenW@gmail.com>
2018-09-09 12:49:54 -0400

    fixing bugs

--- cop4331contacts.com/public_html/js/index.js
+++ cop4331contacts.com/public_html/js/index.js
@@ -99,7 +99,7 @@ function makeContact()
 	document.getElementById("contactAddResult").innerHTML = "";
 
 	var jsonPayload = '{"userId" : ' + userId + ', "firstName" : "' + first + '", "lastName" : "' + last + '", "streetAddress" : "' + streetAdd + '", "city" : "' + cityName + '", "state" : "' + stateName + '", "zip" : "' + zipNum + '", "phone" : "' + phoneNum + '", "email" : "' + emailAdd + '", "birthday" : "' + birthday + '", "notes" : "' + notes + '"}';
-	var url = urlBase + '/AddContact' + extension;
+	var url = urlBase + '/AddContact.' + extension;
 
 	var xhr = new XMLHttpRequest();
 	xhr.open("POST", url, true);
@@ -120,8 +120,8 @@ function makeContact()
 		document.getElementById("contactAddResult").innerHTML = err.message;
 	}
 
-	document.getElementById('firstName').value = firstN.defaultValue;
-	document.getElementById('lastName').value = lastN.defaultValue;
+	document.getElementById('firstN').value = firstN.defaultValue;
+	document.getElementById('lastN').value = lastN.defaultValue;
 	document.getElementById('phone').value = phone.defaultValue;
 	document.getElementById('email').value = email.defaultValue;
 	document.getElementById('streetAddress').value = streetAddress.defaultValue;
