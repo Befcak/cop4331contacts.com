ae123e9c88280f7e42336022e60975b41a34f9b2
Ayleen <islandordaz@gmail.com>
2018-09-10 03:35:32 -0400

    update register()

--- cop4331contacts.com/public_html/js/index.js
+++ cop4331contacts.com/public_html/js/index.js
@@ -169,7 +169,7 @@ function register()
 	var url = urlBase + '/Register.' + extension;
 
 	var xhr = new XMLHttpRequest();
-	xhr.open("POST", url, false);
+	xhr.open("POST", url, true);
 	xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
 	try
 	{
