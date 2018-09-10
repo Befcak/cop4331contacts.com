582d734eef507dbb39d43fd3f23e534b1e6f7f52
Bridget Woodye <BrdgtColleenW@gmail.com>
2018-09-10 08:17:46 -0400

    fixing

--- cop4331contacts.com/public_html/js/index.js
+++ cop4331contacts.com/public_html/js/index.js
@@ -98,7 +98,7 @@ function makeContact()
 
 	document.getElementById("contactAddResult").innerHTML = "";
 
-	alert(first + ', ' + last + ', ' + phoneNum + ', ' + emailAdd + ', ' + streetAdd+ ', ' + cityName + ', ' + stateName + ', ' + zipNum + ', ' + birthday + ', ' + notes);
+	alert(userId + ',' + first + ', ' + last + ', ' + phoneNum + ', ' + emailAdd + ', ' + streetAdd+ ', ' + cityName + ', ' + stateName + ', ' + zipNum + ', ' + birthday + ', ' + notes);
 	var jsonPayload = '{"userID" : "' + userId + '", "firstName" : "' + first + '", "lastName" : "' + last + '", "streetAddress" : "' + streetAdd + '", "city" : "' + cityName + '", "state" : "' + stateName + '", "zip" : "' + zipNum + '", "phone" : "' + phoneNum + '", "email" : "' + emailAdd + '", "birthday" : "' + birthday + '", "notes" : "' + notes + '"}';
 	var url = urlBase + '/AddContact.' + extension;
 
