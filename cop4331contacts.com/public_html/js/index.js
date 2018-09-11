21fe704f728ae63f97c34b4f7131bb50a8f196dc
Vinny <vbrusco30794@gmail.com>
2018-09-11 18:40:51 -0400

    js html update

--- cop4331contacts.com/public_html/js/index.js
+++ cop4331contacts.com/public_html/js/index.js
@@ -289,29 +289,11 @@ function searchContacts()
 				document.getElementById("contactsSearchResult").innerHTML = jsonObject.results[1];
 				document.getElementById("contactsSearchResult").innerHTML = jsonObject.results;
 				console.log(jsonObject.results);
-				    // Declare variables
-				var input, filter, ul, li, a, i;
-				input = document.getElementById('myInput');
-				filter = input.value.toUpperCase();
-				ul = document.getElementById("myUL");
-				li = ul.getElementsByTagName('li');
-
-				li[0].style.display = "test 1";
-				li[1].style.display = "test 2";
-				li[2].style.display = "none";
-				
-				// Loop through all list items, and hide those who don't match the search query
-				//for (i = 0; i < li.length; i++) {
-					
-				//a = li[i].getElementsByTagName("a")[0];
 				
 				
-				//if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
-				//	li[i].style.display = "";
-				//	} else {
-				//		li[i].style.display = "none";
-				//	}
-				//}
+				
+				
+				
 					
 			}
 	 	};
