e86caffb36e8b38913530f6fb09e4658bb51e7eb
Vinny <vbrusco30794@gmail.com>
2018-09-11 17:04:00 -0400

    js update

--- cop4331contacts.com/public_html/js/index.js
+++ cop4331contacts.com/public_html/js/index.js
@@ -301,10 +301,10 @@ function searchContacts()
 				a = li[i].getElementsByTagName("a")[0];
 				if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
 					li[i].style.display = "";
-				} else {
-					li[i].style.display = "none";
-        }
-    }
+					} else {
+						li[i].style.display = "none";
+					}
+				}
 					
 			}
 	 	};
