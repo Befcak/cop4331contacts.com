260e7cda619ad49a44c62f2cd0b197345b62c5f9
Vinny <vbrusco30794@gmail.com>
2018-09-11 16:37:24 -0400

    js test

--- cop4331contacts.com/public_html/js/index.js
+++ cop4331contacts.com/public_html/js/index.js
@@ -269,9 +269,9 @@ function searchContacts()
 				//var jsonObject = JSON.parse( xhr.responseText );
 				document.getElementById("contactsSearchResult").innerHTML = "Names should be below";
 				var jsonObject = JSON.parse( xhr.responseText );
-				document.getElementById("contactsSearchResult").innerHTML = jsonObject.result[0].firstName;
-				document.getElementById("contactsSearchResult").innerHTML = jsonObject.result[0];
-				document.getElementById("contactsSearchResult").innerHTML = jsonObject.result;
+				document.getElementById("contactsSearchResult").innerHTML = jsonObject.result[1];
+				document.getElementById("contactsSearchResult").innerHTML = jsonObject.results[1];
+				document.getElementById("contactsSearchResult").innerHTML = jsonObject.results;
 				console.log(jsonObject.results);
 				var input = document.getElementById("searchText");
 				var ul = document.getElementById("UL");
