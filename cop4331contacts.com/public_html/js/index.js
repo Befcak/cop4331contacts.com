1d97f7e7ac624310afa85d0c03427666e655dafc
Vinny <vbrusco30794@gmail.com>
2018-09-11 17:25:57 -0400

    js test

--- cop4331contacts.com/public_html/js/index.js
+++ cop4331contacts.com/public_html/js/index.js
@@ -296,15 +296,22 @@ function searchContacts()
 				ul = document.getElementById("myUL");
 				li = ul.getElementsByTagName('li');
 
+				li[0].style.display = "test 1";
+				li[1].style.display = "test 2";
+				li[2].style.display = "none";
+				
 				// Loop through all list items, and hide those who don't match the search query
-				for (i = 0; i < li.length; i++) {
-				a = li[i].getElementsByTagName("a")[0];
-				if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
-					li[i].style.display = "";
-					} else {
-						li[i].style.display = "none";
-					}
-				}
+				//for (i = 0; i < li.length; i++) {
+					
+				//a = li[i].getElementsByTagName("a")[0];
+				
+				
+				//if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
+				//	li[i].style.display = "";
+				//	} else {
+				//		li[i].style.display = "none";
+				//	}
+				//}
 					
 			}
 	 	};
