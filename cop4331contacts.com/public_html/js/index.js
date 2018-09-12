564a5b10d68155b99300edde4742a46421d6c182
Bridget Woodye <BrdgtColleenW@gmail.com>
2018-09-12 09:31:55 -0400

    Revert "fixes"
    This reverts commit 8f6b11376208b4a9331640baee912c53ee2b8f5a.

--- cop4331contacts.com/public_html/js/index.js
+++ cop4331contacts.com/public_html/js/index.js
@@ -284,11 +284,11 @@ function searchContacts()
 				var jsonObject = xhr.responseText;
 
 				var i;
-				for( i=1; i<jsonObject.results.length-12; i+=12)
+				for( i=1; i<jsonObject.length-12; i+=12)
 				{
 					var opt = document.createElement("option");
-					alert(jsonObject.results[i]);
-					// opt.text = jsonObject.results[i] + ' ' + jsonObject.results[i];
+					alert(jsonObject.result[i]);
+					// opt.text = jsonObject.results[i] + ' ' + jsonObject.result[i];
 					// opt.value = "";
 					// contactList.options.add(opt);
 				}
