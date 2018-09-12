fc63396803808c9dd37d30d5246a0f5c5a3dc44a
Vinny <vbrusco30794@gmail.com>
2018-09-12 15:51:02 -0400

    update

--- cop4331contacts.com/public_html/js/index.js
+++ cop4331contacts.com/public_html/js/index.js
@@ -348,9 +348,8 @@ if(lastIdClicked >= 1){
 function deleteContact()
 {
 
-
 	var testContactId = 60;
-	var testUserId = 41;
+	//var testUserId = 35;
 	
     if (confirm("Confirm Delete")) {
        document.getElementById("deleteContactResult").innerHTML = "Contact has been deleted!";
@@ -360,7 +359,7 @@ function deleteContact()
 
     
 	
-	var jsonPayload = '{"userId": "'+ testUserId +'","contactId" : "' + testContactId + '"}';
+	var jsonPayload = '{"userId": "'+ userId +'","contactId" : "' + testContactId + '"}';
 	
 	//var jsonPayload = '{"userID": "'+ userId +'","contactID" : "' + lastIdClicked + '"}';
 	var url = urlBase + '/DeleteContactSanitized.' + extension;
@@ -386,5 +385,42 @@ function deleteContact()
 	}
 
 
+	// var testContactId = 60;
+	// var testUserId = 41;
+	//
+  //   if (confirm("Confirm Delete")) {
+  //      document.getElementById("deleteContactResult").innerHTML = "Contact has been deleted!";
+  //   } else {
+  //       return;
+  //   }
+	//
+	//
+	//
+	// var jsonPayload = '{"userId": "'+ testUserId +'","contactId" : "' + testContactId + '"}';
+	//
+	// //var jsonPayload = '{"userID": "'+ userId +'","contactID" : "' + lastIdClicked + '"}';
+	// var url = urlBase + '/DeleteContactSanitized.' + extension;
+	//
+	// var xhr = new XMLHttpRequest();
+	// xhr.open("POST", url, true);
+	// xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+	// try
+	// {
+	// 	xhr.onreadystatechange = function()
+	//  	{
+	//  		if (this.readyState == 4 && this.status == 200)
+	// 		{
+	//
+	// 			document.getElementById("deleteContactResult").innerHTML = "Contact has been deleted!";
+	// 		}
+	//  	};
+	//  	xhr.send(jsonPayload);
+	// }
+	// catch(err)
+	// {
+	// 	document.getElementById("deleteContactResult").innerHTML = err.message;
+	// }
+	//
+	//
 
 }
