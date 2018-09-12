0cd37939ef3d6b324832a8be866ab9af0e7ba061
Bridget Woodye <BrdgtColleenW@gmail.com>
2018-09-12 09:32:21 -0400

    Revert "fixing bugs"
    This reverts commit 5506b4159cd6d2ac6960c50aa56ee4ccf65c562f.

--- cop4331contacts.com/public_html/js/index.js
+++ cop4331contacts.com/public_html/js/index.js
@@ -284,7 +284,7 @@ function searchContacts()
 				var jsonObject = JSON.parse( xhr.responseText );
 
 				var i;
-				for( i=1; i<jsonObject.results.length-12; i+=12)
+				for( i=1; i<jsonObject.results.length; i+=12)
 				{
 					var opt = document.createElement("option");
 					opt.text = jsonObject.results[i] + ' ' + jsonObject.result[i];
